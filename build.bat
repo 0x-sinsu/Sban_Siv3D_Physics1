519a29d7713dbb9441e679d5818c2073f79ad8e5
cc5efd7b0 <157507447+cc5efd7b0@users.noreply.github.com>
2024-02-03 20:54:26 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,12 +1,10 @@
 @echo off
 setlocal
 
-:: MSBuild のパスを取得する（vswhere を使用）
 for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.Component.MSBuild -property installationPath`) do (
     set MSBUILD_PATH=%%i\MSBuild\Current\Bin\MSBuild.exe
 )
 
-:: MSBuild のパスが見つかったかチェック
 if not exist "%MSBUILD_PATH%" (
     echo MSBuild のパスが見つかりませんでした。
     exit /b 1
@@ -15,5 +13,4 @@ if not exist "%MSBUILD_PATH%" (
 :: MSBuild でビルドを実行
 "%MSBUILD_PATH%" SbanPhysics1.sln /p:Configuration=Release /p:Platform="x64"
 
-:: 終了
 endlocal
